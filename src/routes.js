0639fefbcf0cb67a17d3a9861ab8b73f91a0260e
Prashant Madhukar Kale <61052188+prashant0312@users.noreply.github.com>
2022-10-13 21:33:19 +0530

    pk0312

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,8 @@ const router = Router();
 
 
 router.get('/:name/:age', (req, res, next) => {
+    res.status(200).send({
+        message : `Hey ${req.params.name},you are ${req.params.age} years old`
+    });
 })
 module.exports = router;
